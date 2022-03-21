b144ec0937a4cbda7f19b075b819a06ecb2819ce
Peter Nordman <peter.m.nordman@gmail.com>
2022-03-20 20:02:07 -0700

    Next: Add the plants page route

---
+++ next-client/pages/plants.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+import { NextPage } from 'next/types';
+
+const PlantsPage: NextPage = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Search for Plants</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default PlantsPage;
