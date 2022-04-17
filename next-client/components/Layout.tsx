70ed94c6208ebae28c5ecde5f38d9fbe87199fc8
Peter Nordman <peter.m.nordman@gmail.com>
2022-04-17 16:20:45 -0700

    Update Layout wrapper and apply to all pages

--- next-client/components/Layout.tsx
+++ next-client/components/Layout.tsx
@@ -1,13 +1,23 @@
+import Link from 'next/link';
+
 const Layout: React.FC<{}> = ({ children }) => {
   // TODO add a fullWidth prop which removes the width and margin from the container for child components that need access to the full screen width
 
   return (
-    <div className='max-w-3xl mx-auto'>
-      <header>
-        <nav></nav>
+    <div className='flex flex-col min-h-screen max-w-3xl mx-auto'>
+      <header className='py-2'>
+        <nav>
+          <Link href='/' passHref>
+            <a className='block font-bold'>HousePlantr</a>
+          </Link>
+        </nav>
       </header>
-      <main>{children}</main>
-      <footer></footer>
+      <main className='flex-1'>{children}</main>
+      <footer className='text-center p-2'>
+        <Link href='https://github.com/pmnord/fullstackapp' passHref>
+          <a className='text-gray-500 underline '>View Source on GitHub</a>
+        </Link>
+      </footer>
     </div>
   );
 };
